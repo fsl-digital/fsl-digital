aa14133354baf4144de449c425803189d6a6f8fc
Chowdhury, Debajyoti Paul <debajyotipaul.chowdhury@adwmainz.de>
2026-01-01 19:32:58 +0100

    image path fixed

--- src/lib/image-utils.ts
+++ src/lib/image-utils.ts
@@ -1,5 +1,4 @@
 export const getImagePath = (path: string) => {
-  // Remove leading slash if present
   const cleanPath = path.startsWith('/') ? path.slice(1) : path;
-  return `/fsl-digital/${cleanPath}`;
-}; 
+  return `${import.meta.env.BASE_URL}${cleanPath}`;
+};
